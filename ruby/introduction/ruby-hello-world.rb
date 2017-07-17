857f9e44e7aeb86186471b781d1a6a069dc9aa5e
Tong-Kiat Tan <tktan@users.noreply.github.com>
2017-07-17 11:40:17 +0800

    Add ruby-hello-world

---
+++ ruby/introduction/ruby-hello-world.rb
@@ -0,0 +1,11 @@
+# Ruby Tutorial - Hello HackerRank!
+# (by dheeraj)
+
+# Let's get started with Ruby!
+#
+# Go ahead and type the following code in the code-editor:
+# ```
+# print "Hello HackerRank!!"
+# ```
+
+print "Hello HackerRank!!"
